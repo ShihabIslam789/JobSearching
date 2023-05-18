3a023899901407263b8f1162e03c9ad3d626e3be
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-05-17 22:25:20 -0400

    app adjust.

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import { JobSearch } from './JobSearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const jobSearch = new JobSearch('#search-form', '.result-container', '.loading-element');
+jobSearch.setCountryCode();
+jobSearch.configureFormListener();
