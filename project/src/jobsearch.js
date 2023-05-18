73d84cd5cdf43fd7d832b9c473a31e2331fe7cba
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-05-17 22:21:44 -0400

    files

---
+++ project/src/jobsearch.js
@@ -0,0 +1,11 @@
+import { jobTemplate } from './templates';
+import { extractFormData, getCurrencySymbol } from './utils';
+
+
+export class JobSearch {
+
+  constructor(searchFormSelector, resultsContainerSelector, loadingElementSelector) {
+    this.searchForm = document.querySelector(searchFormSelector);
+    this.resultsContainer = document.querySelector(resultsContainerSelector);
+    this.loadingElement = document.querySelector(loadingElementSelector);
+  }
