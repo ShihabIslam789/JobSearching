d8aa1f2a65fa721306795c8f24a80d3fbff7b375
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-05-29 15:01:28 -0400

    function properties.

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -179,3 +179,19 @@ var _templates = require("./templates");
 
 var _utils = require("./utils");
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var JobSearch =
+/*#__PURE__*/
+function () {
+  function JobSearch(searchFormSelector, resultsContainerSelector, loadingElementSelector) {
+    _classCallCheck(this, JobSearch);
+
+    this.searchForm = document.querySelector(searchFormSelector);
+    this.resultsContainer = document.querySelector(resultsContainerSelector);
+    this.loadingElement = document.querySelector(loadingElementSelector);
+  }
